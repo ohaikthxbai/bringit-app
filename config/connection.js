5978d82ae9218aa33345092b0e6930a1514692fc
Roberto Brooks <rob.gate3@gmail.com>
2017-10-18 00:09:44 -0500

    added localhost

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ var connection;
 
 //Set up the connection to the database/Heroku
 connection = mysql.createConnection({
-  host: process.env.AWS_HOST,
-  user: process.env.AWS_USER,
-  password: process.env.AWS_PASSWORD,
-  port: process.env.AWS_PORT,
-  database: process.env.AWS_DB
+  host: process.env.AWS_HOST || '127.0.0.1',
+  user: process.env.AWS_USER || 'root',
+  password: process.env.AWS_PASSWORD || 'root',
+  port: process.env.AWS_PORT || 8889,
+  database: process.env.AWS_DB || 'bringit'
     // host: '127.0.0.1',
     // user: 'root',
     // password: 'root',
